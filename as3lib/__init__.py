d8ce6511d39bd1ad3c8f0f14085e0602a78b5c4e
ajdelguidice <67966644+ajdelguidice@users.noreply.github.com>
2025-10-11 11:26:14 -0500

    Implement a custom importer which supports module tagging
    I did this in order to support classes being in their own files.
    This also remove the as3import function because it isn't needed.

--- as3lib/__init__.py
+++ as3lib/__init__.py
@@ -1,4 +1,4 @@
-from . import as3state, config
+from . import as3state, config, importer
 from pathlib import Path
 from subprocess import check_output
 import os
@@ -146,50 +146,6 @@ def formatToString(obj, objname, *args):
    even in the toplevel section. I'm going to assume that this is a builtin function
    '''
    return ''.join(['[', objname] + [f' {i}={getattr(obj, i)}' for i in args] + [']'])
-
-
-def as3import(packageName: str, namespace, name: str = None):  # This will likely be replace with a proper importlib importer
-   # !Implement * imports
-   '''
-   DO NOT USE THIS YET. I have not decided on the final form this will take as most of it is not implemented yet. The behaviour might change and break things.
-
-   Import implementation similar to actionscript. It functions as described below:
-   All imports are relative to as3lib
-   Will import the object inside of the package with the name of the package (This is currently the best way that I could think of to emulate what actionscript does)
-   namespace must be provided as python's globals are global to each module
-
-   Arguements:
-      packageName - The name and location (with "." as the path separator) of the package. Does not currently support "*" imports
-      namespace - The object in which to import the module into. If "*" is provided as the namespace, name is ignored and the package is returned. EX: if an object called obj is provided, the package will be imported as obj.name
-      name - The name that the module will be imported as. If name is not provided and the package is not a "*" import, the last part of the packageName is used.
-   '''
-   pkg = packageName.split('.')
-   if pkg[-1] == '*':
-      raise NotImplementedError('"*" imports are not implemented yet')
-   else:
-      file = as3state.librarydirectory / ('/'.join(pkg) + '.py')
-      if not file.exists():
-         raise Exception(f'Package "as3lib.{packageName}" does not exist.')
-      if file.is_dir():
-         raise NotImplementedError('Importing directories as packages is not implemented yet.')
-      with open(file, 'rb') as f:
-         b = (b := f.read())[:b.find(b'\n')].split(b' ')
-      if b[0] == b'#?as3package':
-         if len(b) == 1:  # Import the object inside of the file that is the same as the file name
-            package = getattr(__import__(f'as3lib.{".".join(pkg)}', globals(), locals(), (pkg[-1]), 0), pkg[-1])
-            if namespace == '*':
-               return package
-            if isinstance(namespace, dict) and namespace.get('__name__') is not None:  # Is a globals() dict
-               if name is None:
-                  namespace.update({pkg[-1]: package})
-               else:
-                  namespace.update({name: package})
-            elif name is None:
-               setattr(namespace, pkg[-1], package)
-            else:
-               setattr(namespace, name, package)
-         else:  # !When package has specific place to be
-            raise NotImplementedError('Packages with specific locations are not implemented.')
 
 
 # Export toplevel and set up miniamf adapters
---
+++ as3lib/importer.py
@@ -0,0 +1,93 @@
+import sys
+from pathlib import Path
+from importlib.machinery import ModuleSpec
+
+
+# This module is loosely based off of as3lib-miniAMF's util.imports
+
+class ModuleFinder(object):
+   """
+   This module finder/importer implements a special import process for modules
+   tagged with #?as3import. Tagging modules is done by placing the tag at the
+   top of the module right after the shebang line (if included).
+   
+   #?as3import will only import the class inside the module with the same name.
+   Explained simpler, this is like overwritting every instance of
+   "import module" with "from module import class"
+
+   @ivar loaded_modules: C{list} of modules that this finder has seen. Used
+      to stop recursive imports in L{load_module}
+   @ivar special_properties: C{dict} of properties for each module that tags
+      itself.
+   """
+
+   def __init__(self):
+      self.loaded_modules = []
+      self.special_properties = {}
+
+   def find_spec(self, fullname, path, target=None):
+      """
+      Called when an import is made. Retrieves the as3lib tag and sets up the
+      "metadata" for each module.
+
+      @param fullname: The name of the module being imported.
+      @param path: The root path of the module (if a package).
+      @param target: Ignored.
+      @return: If the module is tagged, returns a C{ModuleSpec}. If not, return
+         C{None} to allow the standard import process to continue.
+      """
+      if fullname in self.loaded_modules:
+         return None
+      
+      _type = 0
+
+      if path is not None:
+         filename = fullname.split('.')[-1]
+         p = Path(path[0]) / (filename + '.py')
+         if p.exists():
+            with open(p, 'r') as f:
+               while True:
+                  line = f.readline()
+                  if line.startswith('#!'):  # Skip shebang
+                     continue
+                  elif line.startswith('#?as3import'):  # Only import class with the same name as the module
+                     _type = 1
+                  else:
+                     break
+
+      if _type:
+         self.special_properties[fullname] = [_type, filename]
+         return ModuleSpec(fullname, self)
+
+   def create_module(self, spec):
+      name = spec.name
+      self.loaded_modules.append(name)
+      prop = self.special_properties.get(name, None)
+
+      try:
+         if prop and prop[0] == 1:
+            temp = __import__(name, {}, {}, prop[1])
+            sys.modules[name] = getattr(temp, prop[1])
+      except Exception as e:
+         self.loaded_modules.pop()
+
+         raise e
+
+      return sys.modules[name]
+
+
+def _init():
+   """
+   Internal function to install the module finder.
+   """
+   global finder
+
+   if finder is None:
+      finder = ModuleFinder()
+
+   if finder not in sys.meta_path:
+      sys.meta_path.insert(0, finder)
+
+
+finder = None
+_init()
