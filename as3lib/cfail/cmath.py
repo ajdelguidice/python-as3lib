2c048cd7a8d36477ecaa5419be04cebb50af057f
ajdelguidice <67966644+ajdelguidice@users.noreply.github.com>
2025-10-16 11:03:30 -0500

    [cfail.cmath] Update functions to match c module

--- as3lib/cfail/cmath.py
+++ as3lib/cfail/cmath.py
@@ -1,15 +1,11 @@
-def itk_windowcalculate(neww: float, newh: float, startw: int, starth: int):
+def calculate(neww: float, newh: float, startw: int, starth: int):
    xmult = neww / startw
    ymult = newh / starth
    return ymult if xmult > ymult else xmult
 
 
-def itk_windowresizefont(fo: int, mu: float):
-   return round((fo * mu) / 100)
-
-
-def multdivide(number, multiplier, diviser):
-   return number * multiplier / diviser
+def resizefont(font: int, mult: float):
+   return round((font * mult) / 100)
 
 
 def roundedmultdivide(number, multiplier, diviser):
