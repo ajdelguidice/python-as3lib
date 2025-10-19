9e8274d1eb10f0d59cc32ac738cca0f38282acc6
ajdelguidice <67966644+ajdelguidice@users.noreply.github.com>
2025-10-19 10:53:15 -0500

    [flash.events] Fix most Flake8 complaints and some clean up

--- as3lib/flash/events.py
+++ as3lib/flash/events.py
@@ -2,102 +2,165 @@ from as3lib import metaclasses
 import as3lib as as3
 from copy import copy
 
-#BaseEvent
+
+# BaseEvent
 class _AS3_BASEEVENT:
-   def _getBubbles(self):
-      return self.__bubbles
-   bubbles = property(fget=_getBubbles)
-   def _getCancelable(self):
-      return self.__cancelable
-   cancelable = property(fget=_getCancelable)
-   def _getCTarget(self):
-      return self.__currentTarget
-   currentTarget = property(fget=_getCTarget)
-   def _getEventPhase(self):
-      return self.__eventPhase
-   eventPhase = property(fget=_getEventPhase)
-   def _getTarget(self):
-      return self.__target
-   target = property(fget=_getTarget)
-   def _getType(self):
-      return self.__type
-   type = property(fget=_getType)
-   def __init__(self,type,bubbles=False,cancelable=False,_target=None):
+   @property
+   def bubbles(self):
+      return self._bubbles
+
+   @property
+   def cancelable(self):
+      return self._cancelable
+
+   @property
+   def currentTarget(self):
+      return self._currentTarget
+
+   @property
+   def eventPhase(self):
+      return self._eventPhase
+
+   @property
+   def target(self):
+      return self._target
+
+   @proeprty
+   def type(self):
+      return self._type
+
+   def __init__(self, type, bubbles=False, cancelable=False, _target=None):
       if type not in self._INTERNAL_allowedTypes:
          raise Exception("Provided event type is not valid for this object")
-      self.__type = type
-      self.__bubbles = bubbles
-      self.__cancelable = cancelable
-      self.__currentTarget = None
-      self.__target = _target
-      self.__eventPhase = None
-      self.__preventDefault = False
-   def __eq__(self,value):
+      self._type = type
+      self._bubbles = bubbles
+      self._cancelable = cancelable
+      self._currentTarget = None
+      self._target = _target
+      self._eventPhase = None
+      self._preventDefault = False
+
+   def __eq__(self, value):
       return self.type == value
+
    def __str__(self):
       return self.type
+
    def getEventProperties(self):
-      return (self.type,self.bubbles,self.cancelable,self.currentTarget,self.eventPhase,self.target)
-   def _Internal_setTarget(self,target):
-      """Internal method. do not use"""
-      self.__target = target
-   def _Internal_setCurrentTarget(self,currentTarget):
-      """Internal method. do not use"""
-      self.__currentTarget = currentTarget
-      currentTarget(self.type)
+      return (self.type, self.bubbles, self.cancelable, self.currentTarget, self.eventPhase, self.target)
+
    def clone(self):
       return copy(self)
-   def formatToString(self,className,*arguements):...
+
+   def formatToString(self, className, *arguements):...
+
    def isDefaultPrevented(self):
-      return self.__preventDefault
+      return self._preventDefault
+
    def preventDefault(self):
-      if self.cancelable == True:
-         self.__preventDefault = True
+      if self.cancelable:
+         self._preventDefault = True
+
    def stopImmediatePropagation(self):...
+
    def stopPropagation(self):...
+
    def toString(self):
       return f"[Event type={self.type} bubbles={self.bubbles} cancelable={self.cancelable}]"
 
-#Dummy classes
+
+# Dummy classes
 class Event(_AS3_BASEEVENT):...
 class EventDispatcher:...
 class TextEvent:...
 class ErrorEvent:...
 
-#Interfaces
+# Interfaces
 class IEventDispatcher:
    def __init__(self):
       self.eventobjects = {}
-   def addEventListener(type, listener, useCapture=False, priority=0, useWeakReference=False):...
-   def dispatchEvent(event):...
-   def hasEventListener(type):...
-   def removeEventListener(type, listener, useCapture=False):...
-   def willTrigger(type):...
 
-#Classes
+   def addEventListener(self, type, listener, useCapture=False, priority=0, useWeakReference=False):...
+
+   def dispatchEvent(self, event):...
+
+   def hasEventListener(self, type):...
+
+   def removeEventListener(self, type, listener, useCapture=False):...
+
+   def willTrigger(self, type):...
+
+
+# Classes
 class AccelerometerEvent:...
+
+
 class ActivityEvent:...
+
+
 class AsyncErrorEvent:...
+
+
 class AudioOutputChangeEvent:...
+
+
 class AVDictionaryDataEvent:...
+
+
 class AVHTTPStatusEvent:...
+
+
 class AVPauseAtPeriodEndEvent:...
+
+
 class BrowserInvokeEvent:...
+
+
 class ContextMenuEvent:...
+
+
 class DataEvent:...
+
+
 class DatagramSocketDataEvent:...
+
+
 class DeviceRotationEvent:...
+
+
 class DNSResolverEvent:...
+
+
 class DRMAuthenticateEvent:...
+
+
 class DRMAuthenticateCompleteEvent:...
+
+
 class DRMAuthenticateErrorEvent:...
+
+
 class DRMDeviceGroupErrorEvent:...
+
+
 class DRMErrorEvent:...
+
+
 class DRMLicenseRequestEvent:...
+
+
 class DRMMetadataEvent:...
+
+
 class DRMReturnVoucherCompleteEvent:...
+
+
 class DRMStatusEvent:...
+
+
 class ErrorEvent:...
+
+
 class Event(_AS3_BASEEVENT):
    ACTIVATE = "activate" #bubbles=False, cancelable=False
    ADDED = "added" #bubbles=True, cancelable=False
@@ -157,111 +220,195 @@ class Event(_AS3_BASEEVENT):
    VIDEO_FRAME = "videoFrame" #bubbles=False, cancelable=False
    WORKER_STATE = "workerState" #bubbles=False, cancelable=False
    _INTERNAL_allowedTypes = {"activate","added","addedToStage","browerZoomChange","cancel","change","channelMessage","channelState","clear","close","closing","complete","connect","context3DCreate","copy","cut","deactivate","displaying","enterFrame","exitFrame","exiting","frameConstructed","frameLabel","fullscreen","htmlBoundsChange","htmlDOMInitialize","htmlRender","id3","init","locationChange","mouseLeave","networkChange","open","paste","preparing","removed","removeFromStage","render","resize","scroll","select","selectAll","soundComplete","standardErrorClose","standardInputClose","standardOutputClose","suspend","tabChildrenChange","tabEnableChange","tabIndexChange","textInteractionModeChange","textureReady","unload","userIdle","userPresent","videoFrame","workerState"}
+
+
 class EventDispatcher:
    #!Implement priority, weakReference
-   def __init__(self,target:IEventDispatcher=None):
+
+   def __init__(self, target: IEventDispatcher = None):
       #!Implement target
       self._events = {}
       self._eventsCapture = {}
-   def addEventListener(self,type:str,listener,useCapture:as3.allBoolean=False,priority:as3.allInt=0,useWeakReference:as3.allBoolean=False):
+
+   def addEventListener(self, type: str, listener, useCapture: as3.allBoolean = False, priority: as3.allInt = 0, useWeakReference: as3.allBoolean = False):
       #!Add error
-      if useCapture == False:
-         if self._events.get(type) == None:
+      if useCapture is False:
+         if self._events.get(type) is None:
             self._events[type] = [listener]
          elif listener not in self._events[type]:
             self._events[type].append(listener)
       else:
-         if self._eventsCapture.get(type) == None:
+         if self._eventsCapture.get(type) is None:
             self._eventsCapture[type] = [listener]
          elif listener not in self._eventsCapture[type]:
             self._eventsCapture[type].append(listener)
-   #def _dispatchEventType(self,type_,capture=False):
-   #   """
-   #   This is a temporary function that will be removed later. I just have no idea how to implement the original and haven't implemented event objects yet.
-   #   """
-   #   if capture == False:
-   #      if self._events.get(type_) != None:
-   #         for i in self._events[type_]:
-   #            i(type_)
-   #   else:
-   #      if self._eventsCapture.get(type_) != None:
-   #         for i in self._eventsCapture[type_]:
-   #            i(type_)
-   def dispatchEvent(self,event):
+
+   def dispatchEvent(self, event):
       #!I do not know how to implement useCapture here
       #!Implement stuff to do with bubbles
-      if event.isDefaultPrevented() == False:
-         if self._events.get(event.type) != None:
+      if not event.isDefaultPrevented():
+         if self._events.get(event.type) is not None:
             e = event.clone()
             for i in self._events[event.type]:
-               e._Internal_setCurrentTarget(i)
+               e._currentTarget = i
             return True
       return False
-   def hasEventListener(self,type):
-      if self._events.get(type) != None or self._eventsCapture.get(type) != None:
-         return True
-      return False
-   def removeEventListener(self,type:str,listener,useCapture:as3.allBoolean=False):
-      if useCapture == False:
-         if self._events.get(type) != None:
+
+   def hasEventListener(self, type):
+      return self._events.get(type) is not None or self._eventsCapture.get(type) is not None
+
+   def removeEventListener(self, type: str, listener, useCapture: as3.allBoolean = False):
+      if useCapture is False:
+         if self._events.get(type) is not None:
             try:
                self._events[type].remove(listener)
             except:
                pass
       else:
-         if self._eventsCapture.get(type) != None:
+         if self._eventsCapture.get(type) is not None:
             try:
                self._eventsCapture[type].remove(listener)
             except:
                pass
-   def willTrigger(self,type:str):...
+
+   def willTrigger(self, type: str):...
+
+
 class EventPhase(metaclass=metaclasses._AS3_CONSTANTSOBJECT):
    AT_TARGET = 2
    BUBBLING_PHASE = 3
    CAPTURING_PHASE = 1
+
+
 class FileListEvent:...
+
+
 class FocusEvent:...
+
+
 class FullScreenEvent:...
+
+
 class GameInputEvent:...
+
+
 class GeolocationEvent:...
+
+
 class GestureEvent:...
+
+
 class GesturePhase:...
+
+
 class HTMLUncaughtScriptExceptionEvent:...
+
+
 class HTTPStatusEvent:...
+
+
 class IMEEvent:...
+
+
 class InvokeEvent:...
+
+
 class IOErrorEvent:...
+
+
 class KeyboardEvent:...
+
+
 class LocationChangeEvent:...
+
+
 class MediaEventEvent:...
+
+
 class MouseEventEvent:...
+
+
 class NativeDragEvent:...
+
+
 class NativeProcessExitEvent:...
+
+
 class NativeWindowBoundsEvent:...
+
+
 class NativeWindowDisplayStateEvent:...
+
+
 class NetDataEvent:...
+
+
 class NetMonitorEvent:...
+
+
 class NetStatusEvent:...
+
+
 class OutputProgressEvent:...
+
+
 class PermissionEvent:...
+
+
 class PressAndTapGestureEvent:...
+
+
 class ProgressEvent:...
+
+
 class RemoteNotificationEvent:...
+
+
 class SampleDataEvent:...
+
+
 class ScreenMouseEvent:...
+
+
 class SecurityErrorEvent:...
+
+
 class ServerSocketConnectEvent:...
+
+
 class ShaderEvent:...
+
+
 class SoftKeyboardEvent:...
+
+
 class SoftKeyboardTrigger:...
+
+
 class SQLEvent:...
+
+
 class SQLUpdateEvent:...
+
+
 class StageOrientationEvent:...
+
+
 class StageVideoAvailabilityEvent:...
+
+
 class StageVideoEventEvent:...
+
+
 class StatusEvent:...
+
+
 class StorageVolumeChangeEvent:...
+
+
 class SyncEvent:...
+
+
 class TextEvent(_AS3_BASEEVENT):
    LINK = "link" #bubbles=True, cancelable=False
    TEXT_INPUT = "textInput" #bubbles=True, cancelable=True
@@ -271,22 +418,47 @@ class TextEvent(_AS3_BASEEVENT):
       self.text=text
    def toString(self):
       return f"[TextEvent type={self.type} bubbles={self.value} cancelable={self.cancelable} text={self.text}]"
+
+
 class ThrottleEvent:...
+
+
 class ThrottleType:...
+
+
 class TimerEvent(_AS3_BASEEVENT):
-   TIMER = "timer" #bubbles=False, cancelable=False
-   TIMER_COMPLETE = "timerComplete" #bubbles=False, cancelable=False
-   _INTERNAL_allowedTypes = {"timer","timerComplete"}
-   def __init__(self,type,bubbles=False,cancelable=False,_target=None):
-      super().__init__(type,bubbles,cancelable,_target)
+   TIMER = 'timer'  # bubbles=False, cancelable=False
+   TIMER_COMPLETE = 'timerComplete'  # bubbles=False, cancelable=False
+   _INTERNAL_allowedTypes = {'timer', 'timerComplete'}
+
+   def __init__(self, type, bubbles=False, cancelable=False, _target=None):
+      super().__init__(type, bubbles, cancelable, _target)
+
    def toString(self):
       return f"[TimerEvent type={self.type} bubbles={self.value} cancelable={self.cancelable}]"
+
    def updateAfterEvent(self):...
+
+
 class TouchEvent:...
+
+
 class TouchEventIntent:...
+
+
 class TransformGestureEvent:...
+
+
 class UncaughtErrorEvent:...
+
+
 class UncaughtErrorEvents:...
+
+
 class VideoEvent:...
+
+
 class VideoTextureEvent:...
+
+
 class VsyncStateChangeAvailabilityEvent:...
