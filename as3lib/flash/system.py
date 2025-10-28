44ed8a05cd27e8ce1bc24f4818027d667a6ec2ac
ajdelguidice <67966644+ajdelguidice@users.noreply.github.com>
2025-10-28 08:07:31 -0500

    [flash.system] Formatting

--- as3lib/flash/system.py
+++ as3lib/flash/system.py
@@ -5,151 +5,220 @@ from typing import Union
 import sys
 from functools import cache
 
+
 class ApplicationDomain:...
+
+
 class Capabilities:
-    #!get actual values later
-    #!document changes from original
-    #!use __slots__
-    def _propTrue():
-        return True
-    def _propFalse():
-        return False
-    avHardwareDisable = property(fget=_propTrue) #This is not needed so I have it set to True
-    @cache
-    def _getCPUBits():
-        return as3.Number(platform.architecture()[0][:-3])
-    cpuAddressSize = property(fget=_getCPUBits) #returns 32 (32bit system) or 64 (64bit system)
-    @cache
-    def _getCPUArch():
-        if platform.machine() in {'x86','x86_64','AMD64'}:
-            return 'x86'
-        if platform.machine() == 'PowerPC':
-            return 'PowerPC'
-        if platform.machine() in {'ARM','ARM64'}:
-            return 'ARM'
-    cpuArchitecture = property(fget=_getCPUArch) #returns "PowerPC","x86","SPARC",or "ARM"
-    #hasAccessibility
-    hasAudio = property(fget=_propTrue)
-    #hasAudioEncoder
-    #hasEmbeddedVideo
-    #hasIME
-    #hasMP3
-    #hasPrinting
-    #hasScreenBroadcast
-    #hasScreenPlayback
-    #hasStreamingAudio
-    #hasStreamingVideo
-    #hasTLS
-    #hasVideoEncoder
-    def _getDebug():
-        return as3state.as3DebugEnable
-    isDebugger = property(fget=_getDebug)
-    isEmbeddedInAcrobat = property(fget=_propFalse) #Always false because this is irelavant
-    #language
-    #languages
-    #localFileReadDisable
-    @cache
-    def _getManuf():
-        if as3state.platform == "Windows":
-            return "Adobe Windows"
-        if as3state.platform == "Linux":
-            return "Adobe Linux"
-        if as3state.platform == "Darwin":
-            return "Adobe Macintosh"
-    manufacturer = property(fget=_getManuf)
-    #maxLevelIDC
-    @cache
-    def _getOS():
-        #!add others
-        if as3state.platform == "Windows":...
-        if as3state.platform == "Linux":
-            return f"Linux {platform.release()}"
-        if as3state.platform == "Darwin":...
-    os = property(fget=_getOS)
-    #pixelAspectRatio
-    def _getPlayerType():
-        return "StandAlone"
-    playerType = property(fget=_getPlayerType)
-    #screenColor
-    #screenDPI
-    #screenResolutionX
-    #screenResolutionY
-    #serverString
-    #supports32BitProcesses
-    #supports64BitProcesses
-    #touchscreenType
-    @cache
-    def _getVer():
-        tempfv = as3state.flashVersion
-        if as3state.platform == "Windows":
-            return f"Win {tempfv[0]},{tempfv[1]},{tempfv[2]},{tempfv[3]}"
-        if as3state.platform == "Linux":
-            return f"LNX {tempfv[0]},{tempfv[1]},{tempfv[2]},{tempfv[3]}"
-        if as3state.platform == "Darwin":
-            return f"MAC {tempfv[0]},{tempfv[1]},{tempfv[2]},{tempfv[3]}"
-        if as3state.platform == "Android":
-            return f"AND {tempfv[0]},{tempfv[1]},{tempfv[2]},{tempfv[3]}"
-    version = property(fget=_getVer)
-    def hasMultiChannelAudio(type:str):...
-def fscommand(command, args=""):
-    """
-    This is a simplified version of fscommand and does not do everything it should
-    """
-    if command == "quit":...
-    elif command == "fullscreen":...
-    elif command == "allowscale":...
-    elif command == "showmenu":...
-    elif command == "exec":...
-    elif command == "trapallkeys":...
+   # TODO: get actual values
+   # TODO: document changes from original
+   def _propTrue():
+      return True
+
+   def _propFalse():
+      return False
+
+   avHardwareDisable = property(fget=_propTrue)  # This is not needed so it is always True
+
+   @cache
+   def _getCPUBits():
+      return as3.Number(platform.architecture()[0][:-3])
+
+   cpuAddressSize = property(fget=_getCPUBits)  # returns 32 (32bit system) or 64 (64bit system)
+
+   @cache
+   def _getCPUArch():
+      if platform.machine() in {'x86','x86_64','AMD64'}:
+         return 'x86'
+      if platform.machine() == 'PowerPC':
+         return 'PowerPC'
+      if platform.machine() in {'ARM','ARM64'}:
+         return 'ARM'
+
+   cpuArchitecture = property(fget=_getCPUArch)  # returns 'PowerPC','x86','SPARC',or 'ARM'
+
+   #hasAccessibility
+
+   hasAudio = property(fget=_propTrue)
+
+   #hasAudioEncoder
+   #hasEmbeddedVideo
+   #hasIME
+   #hasMP3
+   #hasPrinting
+   #hasScreenBroadcast
+   #hasScreenPlayback
+   #hasStreamingAudio
+   #hasStreamingVideo
+   #hasTLS
+   #hasVideoEncoder
+
+   def _getDebug():
+      return as3state.as3DebugEnable
+
+   isDebugger = property(fget=_getDebug)
+   isEmbeddedInAcrobat = property(fget=_propFalse)  # Always false because this is irelavant
+
+   #language
+   #languages
+   #localFileReadDisable
+
+   @cache
+   def _getManuf():
+      if as3state.platform == 'Windows':
+         return 'Adobe Windows'
+      if as3state.platform == 'Linux':
+         return 'Adobe Linux'
+      if as3state.platform == 'Darwin':
+         return 'Adobe Macintosh'
+
+   manufacturer = property(fget=_getManuf)
+
+   #maxLevelIDC
+
+   @cache
+   def _getOS():
+      # TODO: add others
+      if as3state.platform == 'Windows':...
+      if as3state.platform == 'Linux':
+         return f'Linux {platform.release()}'
+      if as3state.platform == 'Darwin':...
+
+   os = property(fget=_getOS)
+
+   #pixelAspectRatio
+
+   def _getPlayerType():
+      return 'StandAlone'
+
+   playerType = property(fget=_getPlayerType)
+
+   #screenColor
+   #screenDPI
+   #screenResolutionX
+   #screenResolutionY
+   #serverString
+   #supports32BitProcesses
+   #supports64BitProcesses
+   #touchscreenType
+
+   @cache
+   def _getVer():
+      tempfv = as3state.flashVersion
+      if as3state.platform == 'Windows':
+         return f'Win {tempfv[0]},{tempfv[1]},{tempfv[2]},{tempfv[3]}'
+      if as3state.platform == 'Linux':
+         return f'LNX {tempfv[0]},{tempfv[1]},{tempfv[2]},{tempfv[3]}'
+      if as3state.platform == 'Darwin':
+         return f'MAC {tempfv[0]},{tempfv[1]},{tempfv[2]},{tempfv[3]}'
+      if as3state.platform == 'Android':
+         return f'AND {tempfv[0]},{tempfv[1]},{tempfv[2]},{tempfv[3]}'
+
+   version = property(fget=_getVer)
+
+   def hasMultiChannelAudio(type:str):...
+
+
+def fscommand(command, args=''):
+   '''
+   This is a simplified version of fscommand and does not do everything it should
+   '''
+   if command == 'quit':...
+   elif command == 'fullscreen':...
+   elif command == 'allowscale':...
+   elif command == 'showmenu':...
+   elif command == 'exec':...
+   elif command == 'trapallkeys':...
+
+
 class ImageDecodingPolicy(metaclass=metaclasses._AS3_CONSTANTSOBJECT):
-    ON_DEMAND = "onDemand"
-    ON_LOAD = "onLoad"
+   ON_DEMAND = 'onDemand'
+   ON_LOAD = 'onLoad'
+
+
 class IME:...
+
+
 class IMEConversionMode(metaclass=metaclasses._AS3_CONSTANTSOBJECT):
-    ALPHANUMERIC_FULL = "ALPHANUMERIC_FULL"
-    ALPHANUMERIC_HALF = "ALPHANUMERIC_HALF"
-    CHINESE = "CHINESE"
-    JAPANESE_HIRAGANA = "JAPANESE_HIRAGANA"
-    JAPANESE_KATAKANA_FULL = "JAPANESE_KATAKANA_FULL"
-    JAPANESE_KATAKANA_HALF = "JAPANESE_KATAKANA_HALF"
-    KOREAN = "KOREAN"
-    UNKNOWN = "UNKNOWN"
+   ALPHANUMERIC_FULL = 'ALPHANUMERIC_FULL'
+   ALPHANUMERIC_HALF = 'ALPHANUMERIC_HALF'
+   CHINESE = 'CHINESE'
+   JAPANESE_HIRAGANA = 'JAPANESE_HIRAGANA'
+   JAPANESE_KATAKANA_FULL = 'JAPANESE_KATAKANA_FULL'
+   JAPANESE_KATAKANA_HALF = 'JAPANESE_KATAKANA_HALF'
+   KOREAN = 'KOREAN'
+   UNKNOWN = 'UNKNOWN'
+
+
 class JPEGLoaderContex:...
+
+
 class LoaderContext:...
+
+
 class MessageChannel:...
+
+
 class MessageChannelState(metaclass=metaclasses._AS3_CONSTANTSOBJECT):
-    CLOSED = "closed"
-    CLOSING = "closing"
-    OPEN = "open"
+   CLOSED = 'closed'
+   CLOSING = 'closing'
+   OPEN = 'open'
+
+
 class Security:...
+
+
 class SecurityDomain:...
+
+
 class SecurityPanel:...
+
+
 class System:
-    #freeMemory
-    #ime
-    #privateMemory
-    #totalMemory
-    #totalMemoryNumber
-    #useCodePage
-    def disposeXML():...
-    def exit(code:Union[int,as3.int,as3.uint]=0):
-        sys.exit(int(code))
-    def gc():...
-    def pause():...
-    def pauseForGCIfCollectionImminent():...
-    def resume():...
-    def setClipboard():...
+   #freeMemory
+   #ime
+   #privateMemory
+   #totalMemory
+   #totalMemoryNumber
+   #useCodePage
+
+   def disposeXML():...
+
+   def exit(code:Union[int,as3.int,as3.uint]=0):
+      sys.exit(int(code))
+
+   def gc():...
+
+   def pause():...
+
+   def pauseForGCIfCollectionImminent():...
+
+   def resume():...
+
+   def setClipboard():...
+
+
 class SystemUpdater:...
+
+
 class SystemUpdaterType(metaclass=metaclasses._AS3_CONSTANTSOBJECT):
-    DRM = "drm"
-    SYSTEM = "system"
+   DRM = 'drm'
+   SYSTEM = 'system'
+
+
 class TouchscreenType(metaclass=metaclasses._AS3_CONSTANTSOBJECT):
-    FINGER = "finger"
-    NONE = "none"
-    STYLUS = "stylus"
+   FINGER = 'finger'
+   NONE = 'none'
+   STYLUS = 'stylus'
+
+
 class Worker:...
+
+
 class WorkerDomain:...
+
+
 class WorkerState(metaclass=metaclasses._AS3_CONSTANTSOBJECT):
-    NEW = "new"
-    RUNNING = "running"
-    TERMINATED = "terminated"
+   NEW = 'new'
+   RUNNING = 'running'
+   TERMINATED = 'terminated'
