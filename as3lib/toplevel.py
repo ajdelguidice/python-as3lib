bce83af57594ae3fc3108c416bc101ca193106f9
ajdelguidice <67966644+ajdelguidice@users.noreply.github.com>
2025-12-21 13:24:31 -0600

    [toplevel.py] Fix imports and properly implement the deprecation message

--- as3lib/toplevel.py
+++ as3lib/toplevel.py
@@ -1,20 +1,10 @@
-print('Warning: The as3lib.toplevel module is deprecated. Import as3lib instead.')
-from as3lib._toplevel.Array import *
-from as3lib._toplevel.Boolean import *
-from as3lib._toplevel.Constants import *
-from as3lib._toplevel.Date import *
-from as3lib._toplevel.Errors import *
-from as3lib._toplevel.Functions import *
-from as3lib._toplevel.int import int as Int
-from as3lib._toplevel.JSON import *
-from as3lib._toplevel.Math import *
-from as3lib._toplevel.Namespace import *
-from as3lib._toplevel.Number import *
-from as3lib._toplevel.Object import *
-from as3lib._toplevel.QName import *
-from as3lib._toplevel.RegExp import *
-from as3lib._toplevel.String import *
-from as3lib._toplevel.trace import *
-from as3lib._toplevel.Types import *
-from as3lib._toplevel.uint import *
-from as3lib._toplevel.Vector import *
+from as3lib import (ArgumentError, Array, Boolean, Date, DefinitionError,
+                    Error, EvalError, Int, JSON, Math, Namespace, Number,
+                    Object, QName, RangeError, ReferenceError, RegExp,
+                    SecurityError, String, SyntaxError, TypeError, uint,
+                    URIError, Vector, VerifyError, XML, XMLList, decodeURI,
+                    decodeURIComponent, encodeURI, encodeURIComponent, escape,
+                    isFinite, isNaN, isXMLName, parseFloat, parseInt, trace,
+                    unescape)
+from warnings import warn
+warn('The as3lib.toplevel module is deprecated. Import as3lib instead.', DeprecationWarning)
