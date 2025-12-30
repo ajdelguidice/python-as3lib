5d54ce18f09a9af0cfd2eb46597eeb02f0fda2e2
ajdelguidice <67966644+ajdelguidice@users.noreply.github.com>
2025-12-30 14:48:26 -0600

    [_toplevel.Object] Rewrite isPrototypeOf and make toLocaleString work properly

--- as3lib/_toplevel/Object.py
+++ as3lib/_toplevel/Object.py
@@ -1,5 +1,9 @@
+from warnings import warn
+
 class Object:
    # ActionScript3 Base object
+   prototype = None
+
    def __init__(self):...
 
    def __str__(self):
@@ -8,16 +12,24 @@ class Object:
    def hasOwnProperty(self, name: str):...
 
    def isPrototypeOf(self, theClass):
-      return isinstance(theClass, self.__class__)
+      warn('isPrototypeOf will not work properly because the prototype property is not implemented.')
+      # This should work properly once prototype is implemented properly
+      p = theClass.prototype
+      while p is not None:
+         if p is self.__class__:
+            return True
+         p = p.prototype
+      return False
 
    def propertyIsEnumerable(self, name: str):...
 
    def setPropertyIsEnumerable(self, name: str, isEnum=True):...
 
    def toLocaleString(self):
-      return self.toString()
+      return '[object %s]' % type(self).__name__
 
-   def toString(self):...
+   def toString(self):
+      return '[object %s]' % type(self).__name__
 
    def valueOf(self):
       return self
