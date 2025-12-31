84c0dab6a952cd9df909b133a4c22d05a30a961d
ajdelguidice <67966644+ajdelguidice@users.noreply.github.com>
2025-12-31 11:15:25 -0600

    [tests.__init__] Flake8 formatting and add MethodNotImplemented error

--- as3lib/tests/__init__.py
+++ as3lib/tests/__init__.py
@@ -3,18 +3,23 @@ import unittest
 
 
 class as3libTestCase(unittest.TestCase):
-    def assertIdentical(self, obj1, obj2):
-        if obj1 is not obj2:
-            self.fail('%r is not %r' % (obj1, obj2))
+   def assertIdentical(self, obj1, obj2):
+      if obj1 is not obj2:
+         self.fail('%r is not %r' % (obj1, obj2))
 
-    def assertNaN(self, obj):
-        # NOTE: Relies on NaN being properly set up
-        if not as3lib.isNaN(obj):
-            self.fail('%r is not NaN'% obj)
+   def assertNaN(self, obj):
+      # NOTE: Relies on NaN being properly set up
+      if not as3lib.isNaN(obj):
+         self.fail('%r is not NaN' % obj)
 
-    def assertNaNExact(self, obj):
-        if obj is not as3lib.NaN:
-            self.fail('%r is not NaN'% obj)
+   def assertNaNExact(self, obj):
+      if obj is not as3lib.NaN:
+         self.fail('%r is not NaN' % obj)
+
 
 class TestNotImplemented(NotImplementedError):
-    ...
+   ...
+
+
+class MethodNotImplemented(NotImplementedError):
+   ...
