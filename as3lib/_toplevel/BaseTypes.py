b8537f6a7d8aa60fa91612bd29a3f8c44a30d599
ajdelguidice <67966644+ajdelguidice@users.noreply.github.com>
2026-01-16 11:07:58 -0600

    [_toplevel.BaseTypes] Create file

---
+++ as3lib/_toplevel/BaseTypes.py
@@ -0,0 +1,50 @@
+from __future__ import annotations
+
+
+# Singleton Objects
+class _undefined:
+   __slots__ = ("value")
+
+   def __init__(self):
+      self.value = None
+
+   def __int__(self):
+      return 0
+
+   def __str__(self):
+      return self.toString()
+
+   def __repr__(self):
+      return self.toString()
+
+   def toString(self):
+      return 'undefined'
+
+
+class _null:
+   __slots__ = ("value")
+
+   def __init__(self):
+      self.value = None
+
+   def __int__(self):
+      return 0
+
+   def __str__(self):
+      return self.toString()
+
+   def __repr__(self):
+      return self.toString()
+
+   def __bool__(self):
+      return False
+
+   def toString(self):
+      return 'null'
+
+
+undefined = _undefined()
+null = _null()
+
+
+# Data types
