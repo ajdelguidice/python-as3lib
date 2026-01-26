8aba6543120b55479311d5ec4a0556029b10b80b
ajdelguidice <67966644+ajdelguidice@users.noreply.github.com>
2026-01-25 20:42:39 -0600

    [_toplevel.Types] Fix imports and add warning comment
    This module was used to assist in type annotations but is not really needed any more. It will probably be removed at some point.

--- as3lib/_toplevel/Types.py
+++ as3lib/_toplevel/Types.py
@@ -1,14 +1,16 @@
-import builtins
-from typing import Union
-from types import NoneType
+# WARNING: This file will likely be removed in the future. Python types act
+#          too differenly to work properly in some cases so having these as
+#          annotations can be misleading.
 from as3lib._toplevel.Array import Array
 from as3lib._toplevel.Boolean import Boolean
 from as3lib._toplevel.Constants import undefined, null
-from as3lib._toplevel.int import int
+from as3lib._toplevel.int import int, uint
 from as3lib._toplevel.Number import Number
 from as3lib._toplevel.String import String
-from as3lib._toplevel.uint import uint
 from as3lib._toplevel.Vector import Vector
+import builtins
+from typing import Union
+from types import NoneType
 
 allNumber = Union[builtins.int, float, int, uint, Number]
 allInt = Union[builtins.int, int, uint]
