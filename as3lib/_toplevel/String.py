e19cd058aa05994e99a6b25c5df7031af1827628
ajdelguidice <67966644+ajdelguidice@users.noreply.github.com>
2026-01-29 08:47:05 -0600

    [_toplevel.String] Misc fixes
    Don't import from _toplevel.Functions
    Use % notation instead of f-strings in some places
    Fix calling of unimplemented functions
    Raise NotImplementedError instead of doing nothing

--- as3lib/_toplevel/String.py
+++ as3lib/_toplevel/String.py
@@ -1,10 +1,9 @@
-import builtins
 from as3lib._toplevel.Array import Array
 from as3lib._toplevel.Object import Object
-from as3lib._toplevel.Constants import undefined
-from as3lib._toplevel.Functions import parseInt, parseFloat
+from as3lib._toplevel.Constants import null, undefined
 from as3lib._toplevel.int import int
 from as3lib._toplevel.Number import Number
+import builtins
 
 
 class String(str, Object):
@@ -12,6 +11,7 @@ class String(str, Object):
       self.__init2(self._String(value))
 
    def __init2(self, value):
+      # Workaround for super().__init__() eating arguements
       super().__init__()
 
    def __str__(self):
@@ -28,27 +28,27 @@ class String(str, Object):
          return 'true' if expression else 'false'
       if hasattr(expression, 'toString'):
          return expression.toString()
-      return f'{expression}'
+      return str(expression)
 
    def __repr__(self):
-      return f'as3lib.String({self})'
+      return 'as3lib.String(%s)' % self
 
    def __getitem__(self, item):
       return String(super().__getitem__(item))
 
    def __add__(self, value):
-      return String(f'{self}{self._String(value)}')
+      return String('%s%s' % (self, self._String(value)))
 
    def __bool__(self):
       return self.length > 0
 
    def __neg__(self):
       # TODO: Make sure that this is correct
-      return -parseFloat(self)
+      return -Number(self)
 
    def __pos__(self):
       # TODO: Make sure that this is correct
-      return parseFloat(self)
+      return Number(self)
 
    def charAt(self, index: builtins.int | int = 0):
       if index < 0 or index > len(self) - 1:
@@ -58,33 +58,45 @@ class String(str, Object):
    def charCodeAt(self, index: builtins.int | int = 0):
       if index < 0 or index > len(self) - 1:
          return Number.NaN
-      return parseInt(r'{:04X}'.format(ord(self[index])), 16)
+      return Number(r'{:04X}'.format(ord(self[index])), 16)
 
    def concat(self, *args):
       return self + ''.join([self._String(i) for i in args])
 
-   def fromCharCode():...
+   @staticmethod
+   def fromCharCode(*charCodes):
+      raise NotImplementedError
 
    def indexOf(self, val, startIndex: builtins.int | int = 0):
       return self.find(val, startIndex)
 
-   def lastIndexOf(self, val, startIndex: builtins.int | int = None):...
-   def localeCompare():...
-   def match():...
-   def replace():...
-   def search():...
+   def lastIndexOf(self, val, startIndex: builtins.int | int = None):
+      raise NotImplementedError
+
+   def localeCompare(self, other, *values):
+      raise NotImplementedError
+
+   def match(self, pattern):
+      raise NotImplementedError
+
+   def replace(self, pattern, repl):
+      raise NotImplementedError
+
+   def search(self, pattern):
+      raise NotImplementedError
 
    def slice(self, startIndex=0, endIndex=None):
       if endIndex is None:
          return self[startIndex:]
-      if startIndex < 0:...
+      if startIndex < 0:
+         raise NotImplementedError
       return self[startIndex:endIndex]
 
-   def split(self, delimiter=None, limit=0x7fffffff):
-      if delimiter is undefined or delimiter is None:
+   def split(self, delimiter=null, limit=0x7fffffff):
+      if delimiter is undefined or delimiter is null:
          arr = Array(self)
       elif delimiter == '' or False:  # An empty string, an empty regular expression, or a regular expression that can match an empty string
-         arr = Array(sourceArray=[i for i in self])
+         arr = Array(*[i for i in self])
       elif False:...  # If the delimiter parameter is a regular expression, only the first match at a given position of the string is considered, even if backtracking could find a nonempty substring match at that position.
       elif False:...  # If the delimiter parameter is a regular expression containing grouping parentheses, then each time the delimiter is matched, the results (including any undefined results) of the grouping parentheses are spliced into the output array.
       if limit != 0x7fffffff:
@@ -127,4 +139,4 @@ class String(str, Object):
       return self
 
    def valueOf(self):
-      return f"{self}"
+      return '%s' % self
