c7da9ae7b6346ff415eeee19aa6eda76f15915f0
ajdelguidice <67966644+ajdelguidice@users.noreply.github.com>
2026-01-30 10:22:00 -0600

    [flash.errors] Allow passing id arguement

--- as3lib/flash/errors.py
+++ as3lib/flash/errors.py
@@ -2,66 +2,66 @@ from as3lib import Error
 
 
 class DRMManagerError(Error):
-   def __init__(self, message=''):
-      super().__init__(message)
+   def __init__(self, message='', id=0):
+      super().__init__(message, id)
       self.name = 'DRMManagerError'
 
 
 class EOFError(Error):
-   def __init__(self, message=''):
-      super().__init__(message)
+   def __init__(self, message='', id=0):
+      super().__init__(message, id)
       self.name = 'EOFError'
 
 
 class IllegalOperationError(Error):
-   def __init__(self, message=''):
-      super().__init__(message)
+   def __init__(self, message='', id=0):
+      super().__init__(message, id)
       self.name = 'IllegalOperationError'
 
 
 class InvalidSWFError(Error):
-   def __init__(self, message=''):
-      super().__init__(message)
+   def __init__(self, message='', id=0):
+      super().__init__(message, id)
       self.name = 'InvalidSWFError'
 
 
 class IOError(Error):
-   def __init__(self, message=''):
-      super().__init__(message)
+   def __init__(self, message='', id=0):
+      super().__init__(message, id)
       self.name = 'IOError'
 
 
 class MemoryError(Error):
-   def __init__(self, message=''):
-      super().__init__(message)
+   def __init__(self, message='', id=0):
+      super().__init__(message, id)
       self.name = 'MemoryError'
 
 
 class PermissionError(Error):
-   def __init__(self, message=''):
-      super().__init__(message)
+   def __init__(self, message='', id=0):
+      super().__init__(message, id)
       self.name = 'PermissionError'
 
 
 class ScriptTimeoutError(Error):
-   def __init__(self, message=''):
-      super().__init__(message)
+   def __init__(self, message='', id=0):
+      super().__init__(message, id)
       self.name = 'ScriptTimeoutError'
 
 
 class SQLError(Error):
-   def __init__(self, message=''):
-      super().__init__(message)
+   def __init__(self, message='', id=0):
+      super().__init__(message, id)
       self.name = 'SQLError'
 
 
 class SQLErrorOperation(Error):
-   def __init__(self, message=''):
-      super().__init__(message)
+   def __init__(self, message='', id=0):
+      super().__init__(message, id)
       self.name = 'SQLErrorOperation'
 
 
 class StackOverflowError(Error):
-   def __init__(self, message=''):
-      super().__init__(message)
+   def __init__(self, message='', id=0):
+      super().__init__(message, id)
       self.name = 'StackOverflowError'
