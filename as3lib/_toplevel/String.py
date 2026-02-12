163856e4bb8b504f14e3fdfc6737b8a83fd59a8d
ajdelguidice <67966644+ajdelguidice@users.noreply.github.com>
2026-02-12 08:45:01 -0600

    [_toplevel.String] Attemp to fix a couple of methods

--- as3lib/_toplevel/String.py
+++ as3lib/_toplevel/String.py
@@ -1,8 +1,8 @@
 from as3lib._toplevel.Array import Array
-from as3lib._toplevel.Object import Object
 from as3lib._toplevel.Constants import null, undefined
 from as3lib._toplevel.int import int
 from as3lib._toplevel.Number import Number
+from as3lib._toplevel.Object import Object
 import builtins
 
 
@@ -58,7 +58,7 @@ class String(str, Object):
    def charCodeAt(self, index: builtins.int | int = 0):
       if index < 0 or index > len(self) - 1:
          return Number.NaN
-      return Number(r'{:04X}'.format(ord(self[index])), 16)
+      return Number(builtins.int(r'{:04X}'.format(ord(self[index])), 16))
 
    def concat(self, *args):
       return self + ''.join([self._String(i) for i in args])
@@ -68,10 +68,10 @@ class String(str, Object):
       raise NotImplementedError
 
    def indexOf(self, val, startIndex: builtins.int | int = 0):
-      return self.find(val, startIndex)
+      return self.find(String(val), startIndex)
 
-   def lastIndexOf(self, val, startIndex: builtins.int | int = None):
-      raise NotImplementedError
+   def lastIndexOf(self, val, startIndex: builtins.int | int = 0x7fffffff):
+      return self.rfind(String(val), startIndex)
 
    def localeCompare(self, other, *values):
       raise NotImplementedError
@@ -82,41 +82,60 @@ class String(str, Object):
    def replace(self, pattern, repl):
       raise NotImplementedError
 
-   def search(self, pattern):
+   def search(self, pattern = undefined):
+      if pattern is undefined or pattern is null:
+         return -1
       raise NotImplementedError
 
-   def slice(self, startIndex=0, endIndex=None):
-      if endIndex is None:
-         return self[startIndex:]
-      if startIndex < 0:
-         raise NotImplementedError
-      return self[startIndex:endIndex]
+   def slice(self, startIndex=0, endIndex=null):
+      si, ei = Number(startIndex), Number(endIndex)
+      if si == Number.POSITIVE_INFINITY or ei == Number.NEGATIVE_INFINITY:
+         return String('')
+      si = int(si)
+      if endIndex is null or ei == Number.POSITIVE_INFINITY:
+         return self[si:]
+      ei = int(ei)
+      return self[si:ei]
 
    def split(self, delimiter=null, limit=0x7fffffff):
       if delimiter is undefined or delimiter is null:
-         arr = Array(self)
-      elif delimiter == '' or False:  # An empty string, an empty regular expression, or a regular expression that can match an empty string
-         arr = Array(*[i for i in self])
-      elif False:...  # If the delimiter parameter is a regular expression, only the first match at a given position of the string is considered, even if backtracking could find a nonempty substring match at that position.
-      elif False:...  # If the delimiter parameter is a regular expression containing grouping parentheses, then each time the delimiter is matched, the results (including any undefined results) of the grouping parentheses are spliced into the output array.
-      if limit != 0x7fffffff:
-         return arr[:limit]
-      return arr
+         return Array(self)
+      elif delimiter == '' or False:
+         # An empty string, an empty regular expression, or a regular
+         # expression that can match an empty string
+         return Array(*[i for i in self])
+      elif False:
+         # If the delimiter parameter is a regular expression, only the first
+         # match at a given position of the string is considered, even if
+         # backtracking could find a nonempty substring match at that
+         # position.
+         ...
+      elif False:
+         # If the delimiter parameter is a regular expression containing
+         # grouping parentheses, then each time the delimiter is matched, the
+         # results (including any undefined results) of the grouping
+         # parentheses are spliced into the output array.
+         ...
+      else:
+         return Array(*super().split(delimiter, limit))
 
-   def substr(self, startIndex: builtins.int | int = 0, len: builtins.int | int = None):
-      if len < 0:
-         len = 0
-      if startIndex < 0:
-         startIndex = self.length + startIndex
-      if len is None:
+   def substr(self, startIndex: Number = 0, len: Number = null):
+      startIndex = Number(startIndex)
+      if len is null:
          return self[startIndex:]
+      else:
+         len = Number(len)
+      if len == Number.NEGATIVE_INFINITY:
+         return String('')
+      if len < 0:
+         len += self.length
       return self[startIndex:startIndex+len]
 
-   def substring(self, startIndex: builtins.int | int = 0, endIndex: builtins.int | int = None):
+   def substring(self, startIndex: Number = 0, endIndex: Number = null):
+      startIndex = Number(startIndex)
       if startIndex < 0:
          startIndex = 0
-      if endIndex is None:
-         endIndex = len(self)
+      endIndex = self.length if endIndex is null else Number(endIndex)
       if endIndex < 0:
          endIndex = 0
       if startIndex > endIndex:
