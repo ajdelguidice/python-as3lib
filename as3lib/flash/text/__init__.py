705b23865a2ef1368a571e59cafa7a17c81f26e7
ajdelguidice <67966644+ajdelguidice@users.noreply.github.com>
2026-02-23 11:43:58 -0600

    [flash.text] Convert into a folder
    This allows flash.text.engine and flash.text.ime to be separate files.
    
    Other changes:
    - Create stubs for text.engine and text.ime
    - Stub text.Font, text.FontStyle, text.FontType

---
+++ as3lib/flash/text/__init__.py
@@ -0,0 +1,97 @@
+from __future__ import annotations
+from as3lib.metaclasses import _AS3_CONSTANTSOBJECT
+from as3lib import Object
+from . import engine, ime
+
+
+class AntiAliasType:...
+
+
+class AutoCapitalize:...
+
+
+class CSMSettings:...
+
+
+class Font(Object):
+   def __init__(self):
+      # From the swf files that I've seen, flash seems to register the font without a manual call to registerFont. I'm unsure if it is done here.
+      Font.registerFont(self)
+
+   @staticmethod
+   def enumerateFonts(enumerateDeviceFonts=False):...
+
+   def hasGlyphs(self, str):...
+
+   @staticmethod
+   def registerFont(font):...
+
+
+class FontStyle(_AS3_CONSTANTSOBJECT):
+   BOLD = 'bold'
+   BOLD_ITALIC = 'boldItalic'
+   ITALIC = 'italic'
+   REGULAR = 'regular'
+
+
+class FontType(_AS3_CONSTANTSOBJECT):
+   DEVICE = 'device'
+   EMBEDDED = 'embedded'
+   EMBEDDED_CFF = 'embeddedCFF'
+
+
+class GridFitType:...
+
+
+class ReturnKeyLabel:...
+
+
+class SoftKeyboardType:...
+
+
+class StageText:...
+
+
+class StageTextClearButtonMode:...
+
+
+class StageTextInitOptions:...
+
+
+class StaticText:...
+
+
+class StyleSheet:...
+
+
+class TextColorType:...
+
+
+class TextDisplayMode:...
+
+
+class TextField:...
+
+
+class TextFieldAutoSize:...
+
+
+class TextFieldType:...
+
+
+class TextFormat:...
+
+
+class TextFormatAlign:...
+
+
+class TextInteractionMode:...
+
+
+class TextLineMetrics:...
+
+
+class TextRenderer:...
+
+
+class TextSnapshot:...
