79ad6674348f01daa3c151909f912eed983922c6
ajdelguidice <67966644+ajdelguidice@users.noreply.github.com>
2026-02-23 08:56:57 -0600

    [_toplevel.JSON] Fix formatting

--- as3lib/_toplevel/JSON.py
+++ as3lib/_toplevel/JSON.py
@@ -5,8 +5,8 @@ from as3lib._toplevel.Object import Object
 class JSON(Object):
    @staticmethod
    def parse(text, reviver=null):
-         raise NotImplementedError
+      raise NotImplementedError
 
    @staticmethod
    def stringify(value, replacer=null, space=null):
-         raise NotImplementedError
+      raise NotImplementedError
