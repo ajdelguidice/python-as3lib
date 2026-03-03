8819993c81a054943aa50703605dd6ce97b2660c
ajdelguidice <67966644+ajdelguidice@users.noreply.github.com>
2026-03-03 09:11:32 -0600

    Move XML and Vector

--- as3lib/_toplevel/__init__.py
+++ as3lib/_toplevel/__init__.py
@@ -3,7 +3,7 @@ from as3lib.helpers import recursionDepth
 import builtins
 from ctypes import c_double, c_uint32, c_int32
 import datetime, time
-from functools import cmp_to_key
+from functools import cmp_to_key, partial
 from inspect import isfunction
 from io import StringIO
 import math
@@ -1683,6 +1683,548 @@ class JSON(Object):
    @staticmethod
    def stringify(value, replacer=null, space=null):
       raise NotImplementedError
+
+
+class Vector(list, Object):
+   '''
+   AS3 Vector datatype.
+
+   This class is not really a vector as I haven't found a way to do that in
+   python. It is instead just a type locked list.
+
+   I have not found a way to create a syntax similar to Vector.<T> so you
+   currently have to declare it like Vector(..., type=T). The way this is
+   currently handled also does not allow you to use Vector.<T> as a type.
+   '''
+   @staticmethod
+   def coercePythonToAs3Object(obj, type_):
+      # bool must go above int because bool isinstance of int
+      if isinstance(obj, bool):
+         return Boolean(obj)
+      if isinstance(obj, builtins.int):
+         if type_ is int:
+            return int(obj)
+         if type_ is uint:
+            return uint(obj)
+         return Number(obj)
+      if isinstance(obj, float):
+         return Number(obj)
+      if isinstance(obj, str):
+         return String(obj)
+
+      # Could not coerce object or object already as3
+      return obj
+
+   @staticmethod
+   def _checkTypeAll(arr, type_, superclass):
+      # TODO: Implements/Implementer
+      for i in each(arr):
+         Vector._checkType(i, type_, superclass)
+
+   @staticmethod
+   def _checkType(value, type_, superclass):
+      # TODO: Implements/Implementer
+      if value is not null:
+         if superclass:
+            if not isinstance(value, type_):
+               raise TypeError('%s is not %s or subclass of %s' % (type(value), type_, type_))
+         else:
+            if type(value) is not type_:
+               raise TypeError('%s is not %s' % (type(value), type_))
+
+   def __init__(self, length=0, fixed=False, **kwargs):
+      if isinstance(length, list):  # Function behaviour
+         # TODO: Make sure this works properly
+         if isinstance(length, Vector):
+            self._fixed = length.fixed
+            self._superclass = length._superclass
+         self._type = kwargs['type']
+         self._fixed = False
+         self._superclass = True
+         length = [Vector.coercePythonToAs3Object(i, self._type) for i in each(length)]
+         Vector._checkTypeAll(length, self._type, self._superclass)
+         super().__init__(length)
+      else:  # Constructor behaviour
+         self._type = kwargs['type']
+         self._superclass = False
+         self._fixed = fixed
+         super().__init__((null for i in range(length)))
+
+   def __iter__(self):
+      return (i for i in range(len(self)))
+
+   def __each__(self):
+      return (self[i] for i in range(len(self)))
+
+   def extend(self, iterable):
+      if self.fixed:
+         raise RangeError('Can not change vector length while fixed is set to true.')
+      super().extend(each(iterable))
+
+   @property
+   def fixed(self):
+      return self._fixed
+
+   @fixed.setter
+   def fixed(self, value):
+      self._fixed = value
+
+   @property
+   def length(self):
+      return len(self)
+
+   @length.setter
+   def length(self, value):
+      if self.fixed:
+         raise RangeError('Can not set vector length while fixed is set to true.')
+      if value > 4294967296:
+         raise RangeError('New length outside of accepted range (0-4294967296).')
+      if len(self) > value:
+         while len(self) > value:
+            self.pop()
+      elif len(self) < value:
+         while len(self) < value:
+            self.append(null)
+
+   @staticmethod
+   def _join(o, sep=None):
+      if sep is None or sep is undefined:
+         s = ','
+      elif hasattr(sep, 'toString'):
+         s = sep.toString()
+      else:
+         s = str(sep)
+      with StringIO() as out:
+         for i in o:
+            x = o[i]
+            if x != None:
+               out.write(str(x))
+            if i + 1 < o.length:
+               out.write(s)
+         return out.getvalue()
+
+   def __repr__(self):
+      return 'as3lib.Vector.<%s>(%s)' % (self._type.__name__, self)
+
+   def __getitem__(self, item):
+      return super().__getitem__(item)
+
+   def __setitem__(self, item, value):
+      value = Vector.coercePythonToAs3Object(value, self._type)
+      Vector._checkType(value, self._type, self._superclass)
+      super().__setitem__(item, value)
+
+   def concat(self, *args):
+      temp = Vector(self, type=self._type)
+      if len(args) > 0:
+         for i in args:
+            if isinstance(i, Vector) and issubclass(i._type, self._type):
+               temp.extend(i)
+            elif not isinstance(i, Vector):
+               raise TypeError('Vector.concat; One or more arguements are not of type Vector')
+            else:
+               raise TypeError('Vector.concat; One or more arguements do not have a base type that can be converted to the current base type.')
+      temp.fixed = self.fixed
+      return temp
+
+   def every(self, callback, thisObject=null):
+      if callback is null:
+         return True
+      for i in self:
+         if not callback(self[i], i, self):
+            return False
+      return True
+
+   def filter(self, callback, thisObject=null):
+      # TODO: Handle null callback
+      tempVector = Vector(type=self._type)
+      tempVector._superclass = self._superclass
+      for i in self:
+         if callback(self[i], i, self):
+            tempVector.push(item)
+      return tempVector
+
+   def forEach(self, callback, thisObject=null):
+      if callback is null:
+         return undefined
+      for i in self:
+         callback(self[i], i, self)
+
+   def indexOf(self, searchElement, fromIndex=0):
+      if fromIndex < 0:
+         fromIndex = len(self) - fromIndex
+      for i in range(fromIndex, len(self)):
+         if self[i] == searchElement:
+            return i
+      return -1
+
+   def insertAt(self, index, element):
+      if self.fixed:
+         raise RangeError('insertAt can not be called on a Vector with fixed set to true.')
+      elif self._superclass:
+         if element is null or isinstance(element, self._type):
+            raise NotImplementedError
+      else:
+         raise NotImplementedError
+
+   def join(self, sep: str = ','):
+      return Vector._join(self, sep)
+
+   def lastIndexOf(self, searchElement, fromIndex=None):
+      if fromIndex is None:
+         fromIndex = len(self)
+      elif fromIndex < 0:
+         fromIndex = len(self) - fromIndex
+      raise NotImplementedError
+      # index = self[::-1].indexOf(searchElement,len(self)-1-fromIndex)
+      # return index if index == -1 else len(self)-1-index
+
+   def map(self, callback, thisObject=null):
+      # TODO: Handle null callback
+      tempVect = Vector(self.length, type=self._type)
+      tempVect._superclass = self._superclass
+      for i in self:
+         tempVect[i] = callback(self[i], i, self)
+      return tempVect
+
+   def pop(self):
+      if self.fixed:
+         raise RangeError('pop can not be called on a Vector with fixed set to true.')
+      return super().pop(-1)
+
+   def push(self, *args):
+      if self.fixed:
+         raise RangeError('push can not be called on a Vector with fixed set to true.')
+      # !Check item types
+      self.extend(args)
+      return len(self)
+
+   def removeAt(self, index):
+      if self.fixed:
+         raise RangeError('removeAt can not be called on a Vector with fixed set to true.')
+      elif False:  # !Index out of bounds
+         raise RangeError('index is out of bounds.')
+      return super().pop(index)
+
+   def reverse(self):
+      super().reverse()
+      return self
+
+   def shift(self):
+      if self.fixed:
+         raise RangeError('shift can not be called on a Vector with fixed set to true.')
+      return super().pop(0)
+
+   def slice(self):
+      raise NotImplementedError
+
+   def some(self, callback, thisObject=null):
+      if callback is null:
+         return False
+      for i in self:
+         if callback(self[i], i, self):
+            return True
+      return False
+
+   def sort(self):
+      raise NotImplementedError
+
+   def splice(self):
+      raise NotImplementedError
+
+   def toLocaleString(self):
+      raise NotImplementedError
+
+   def toString(self):
+      return Vector._join(self)
+
+   def unshift(self, *args):
+      if self.fixed:
+         raise RangeError('unshift can not be called on a Vector with fixed set to true.')
+      l = []
+      for i in args:
+         l.append(Vector.coercePythonToAs3Object(i, self._type))
+         Vector._checkType(l[-1], self._type, self._superclass)
+      tempVect = (*l, *each(self))
+      self.clear()
+      self.extend(tempVect)
+      return len(self)
+
+# Temporary aliases. Remove when a syntax similar to Vector.<Type> is
+# implemented.
+Vector.Boolean = partial(Vector, type=Boolean)
+Vector.int = partial(Vector, type=int)
+Vector.Number = partial(Vector, type=Number)
+Vector.uint = partial(Vector, type=uint)
+Vector.String = partial(Vector, type=String)
+
+
+class Namespace(Object):
+   @property
+   def prefix(self):
+      return self._prefix
+
+   @prefix.setter
+   def prefix(self, value):
+      self._prefix = value
+
+   @property
+   def uri(self):
+      return self._uri
+
+   @uri.setter
+   def uri(self, value):
+      self._uri = value
+
+   def __init__(self, *args):
+      # Fix enumeration order
+      self._uri = undefined
+      self._prefix = undefined
+
+      if len(args) >= 2:
+         val1 = args[0]
+         val2 = args[1]
+         if val1 is null:
+            val1 = undefined
+         if isXMLName(val1):
+            self._prefix = String(val1)
+         else:
+            self._prefix = undefined
+         if isinstance(val2, QName):
+            self._uri = val2.uri
+         elif self.prefix != '' and isinstance(val2, str) and not len(val2):
+            raise TypeError('Illegal prefix %s for no namespace.' % self.prefix, 1098)
+         else:
+            self._uri = String(val2)
+      elif len(args):
+         val = args[0]
+         if isinstance(val, Namespace):
+            self._prefix = val.prefix
+            self._uri = val.uri
+         elif isinstance(val, QName):
+            self._prefix = undefined
+            self._uri = val.uri
+         elif isinstance(val, str) and val == '':
+            self._prefix = String()
+            self._uri = String()
+         else:
+            self._prefix = undefined
+            self._uri = String(val)
+      else:
+         self._prefix = String()
+         self._uri = String()
+
+   def toString(self):
+      return self.uri
+
+   def valueOf(self):
+      return self.uri
+
+
+class QName(Object):
+   @property
+   def localName(self):
+      return self._localName
+
+   @property
+   def uri(self):
+      return self._uri
+
+   def __init__(self, *args):
+      self._uri = null
+      if len(args) >= 2:
+         uri = args[0]
+         localName = args[1]
+         if isinstance(uri, Namespace):
+            self._uri = uri.uri
+         elif uri is not null:
+            self._uri = String(uri)
+         if isinstance(localName, QName):
+            self._localName = localName.localName
+         else:
+            self._localName = String(localName)
+      elif len(args):
+         qname = args[0]
+         if isinstance(qname, QName):
+            self._localName = qname.localName
+            self._uri = qname.uri
+         elif qname is undefined or qname is null:
+            self._localName = String()
+         else:
+            self._localName = String(qname)
+      else:
+         self._localName = String()
+
+   def toString(self):
+      if self.uri == '':
+         return self.localName
+      elif self.uri is null:
+         return '*::%s' % self.localName
+      return '%s::%s' % (self.uri, self.localName)
+
+   def valueOf(self):
+      return self
+
+
+class XMLList(Object):
+   # TODO: Check to see if any of these functions should return a flattened list
+   def __init__(self, value):
+      self._value = Array()
+      ...
+
+   def attribute(self, attributeName):
+      res = Array()
+      for i in each(self._value):
+         j = i.attributes(attributeName)
+         if j.length() > 0:
+            res.append(j)
+      return XMLList(res)
+
+   def attributes(self):
+      return XMLList([i.attributes() for i in each(self._value)])
+
+   def child(self, propertyName):
+      return XMLList([i.child(propertyName) for i in each(self._value)])
+
+   def children(self):
+      return XMLList([i.children() for i in each(self._value)])
+
+   def comments(self):...
+   def contains(self, value):...
+   def copy(self):...
+   def decendants(self, name):...
+   def elements(self, name):...
+   def hasComplexContext(self):...
+   def hasOwnProperty(self, p):...
+   def hasSimpleContext(self):...
+
+   def length(self):
+      return self._value.length
+
+   def normalize(self):...
+   def parent(self):...
+   def processingInstructions(self, name):...
+   def propertyIsEnumerable(self, p):...
+
+   def text(self):
+      return XMLList([i.text() for i in self._value])
+
+   def toString(self):...
+   def toXMLString(self):...
+
+   def valueOf(self):
+      return self
+
+
+class XML(Object):
+   # Prerequisite: Object attribute access
+   # TODO: Implement accessing children. This should be done by using <xmlobj>.<child>. Doing this appears to return all children with the name <child>
+   ignoreComments = True
+   ignoreProcessingInstructions = True
+   ignoreWhitespace = True
+   prettyIndent = 2
+   prettyPrinting = True
+
+   def __init__(self, value):
+      self._name: QName = None
+      self._type = None  # INTERNAL: Node type (text, comment, processing-instruction, attribute, or element)
+      self._namespace = None
+      self._namespaces = Array()
+      ...
+
+   def addNamespace(self, ns):...
+   def appendChild(self, child):...
+   def attribute(self, attributeName):...
+   def attributes(self):...
+   def child(self, propertyName):...
+   def childIndex(self):...
+   def children(self):...
+   def comments(self):...
+   def contains(self, value):...
+   def copy(self):...
+
+   @staticmethod
+   def defaultSettings():
+      obj = Object()
+      obj.ignoreComments = true
+      obj.ignoreProcessingInstructions = true
+      obj.ignoreWhitespace = true
+      obj.prettyIndent = int(2)
+      obj.prettyPrinting = true
+      return obj
+
+   def decendants(self, name):...
+   def elements(self, name):...
+   def hasComplexContext(self):...
+   def hasOwnProperty(self, p):...
+   def hasSimpleContext(self):...
+   def inScopeNamespace(self):...
+   def inserChildAfter(self, child1, child2):...
+   def insertChildBefore(self, child1, child2):...
+
+   def length(self):
+      return 1
+
+   def localName(self):
+      return self._name.localName
+
+   def name(self):
+      return self._name
+
+   def namespace(prefix):...
+   def namespaceDeclarations(self):...
+
+   def nodeKind(self):
+      return self._type
+
+   def normalize(self):...
+   def parent(self):...
+   def prependChild(self):...
+   def processingInstructions(self, name):...
+   def propertyIsEnumerable(self, p):...
+   def removeNamespace(self, ns):...
+   def replace(self, propertyName, value):...
+   def setChildren(self, value):...
+
+   def setLocalName(self, name):
+      self._name._localName = name
+
+   def setName(self, name):...
+
+   def setNamespace(self, ns:Namespace):
+      self._namespace = ns
+
+   @staticmethod
+   def setSettings(rest = null):
+      if rest is null:
+         rest = XML.defaultSettings()
+      if 'ignoreComments' in rest:
+         XML.ignoreComments = rest.ignoreComments
+      if 'ignoreProcessingInstructions' in rest:
+         XML.ignoreProcessingInstructions = rest.ignoreProcessingInstructions
+      if 'ignoreWhitespace' in rest:
+         XML.ignoreWhitespace = rest.ignoreWhitespace
+      if 'prettyIndent' in rest:
+         XML.prettyIndent = rest.prettyIndent
+      if 'prettyPrinting' in rest:
+         XML.prettyPrinting = rest.prettyPrinting
+
+   @staticmethod
+   def settings():
+      obj = Object()
+      obj.ignoreComments = XML.ignoreComments
+      obj.ignoreProcessingInstructions = XML.ignoreProcessingInstructions
+      obj.ignoreWhitespace = XML.ignoreWhitespace
+      obj.prettyIndent = XML.prettyIndent
+      obj.prettyPrinting = XML.prettyPrinting
+      return obj
+
+   def text(self):...
+   def toString(self):...
+   def toXMLString(self):...
+
+   def valueOf(self):
+      return self
 
 
 # Functions
