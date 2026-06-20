83fef91721f11e51b15da520091987041516ea45
ajdelguidice <67966644+ajdelguidice@users.noreply.github.com>
2026-06-19 21:37:50 -0500

    [_toplevel.Keywords, _toplevel.trace] Use indentsize 4

--- as3lib/_toplevel/Keywords.py
+++ as3lib/_toplevel/Keywords.py
@@ -5,49 +5,50 @@ from miniamf import register_package
 
 @dataclass
 class amfMetaData:
-   # The variable names for this are defined by miniamf's __amf__ property
-   static_attrs: list
-   exclude_attrs: list
-   readonly_attrs: list
-   proxy_attrs: list
-   amf3: bool
-   dynamic: bool
-   alias: bool  # TODO: Placeholder data type
-   external: bool
-   synonym_attrs: list
+    # The variable names for this are defined by miniamf's __amf__ property
+    static_attrs: list
+    exclude_attrs: list
+    readonly_attrs: list
+    proxy_attrs: list
+    amf3: bool
+    dynamic: bool
+    alias: bool  # TODO: Placeholder data type
+    external: bool
+    synonym_attrs: list
 
 
 @dataclass
 class as3PackageMetaData:
-   namespace: str
-   ...
+    namespace: str
+    ...
 
 
 class extends:
-   def __init__(self, parent):
-      self._p = parent
+    def __init__(self, parent):
+        self._p = parent
 
-   def __call__(self, cls):
-      cls.prototype = self._p
-      return cls
+    def __call__(self, cls):
+        cls.prototype = self._p
+        return cls
 
 
-class package:...
+class package:
+    ...
 
 
 class implements:
-   def __init__(self, *interfaces):
-      self._i = interfaces
+    def __init__(self, *interfaces):
+        self._i = interfaces
 
-   def __call__(self, cls):
-      cls._as3_implements = self._i
+    def __call__(self, cls):
+        cls._as3_implements = self._i
 
 
 class namespace:
-   # Currently only works on packages
-   def __init__(self, ns):
-      self.ns = ns
+    # Currently only works on packages
+    def __init__(self, ns):
+        self.ns = ns
 
-   def __call__(self, cls):
-      register_package(cls, self.ns)
-      return cls
+    def __call__(self, cls):
+        register_package(cls, self.ns)
+        return cls
