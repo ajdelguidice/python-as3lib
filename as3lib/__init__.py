120d727e357f8d9f68a6c229088a8f7952a5409b
ajdelguidice <67966644+ajdelguidice@users.noreply.github.com>
2026-07-18 11:43:58 -0500

    [__init__]

--- as3lib/__init__.py
+++ as3lib/__init__.py
@@ -1,8 +1,9 @@
-from . import as3state, config
+from . import as3state, TOML
 from ._toplevel.trace import trace
 from .helpers import isValidDirectory
-import builtins, os
+import builtins, configparser, os
 from functools import partial
+from importlib.util import find_spec
 from miniamf import add_type
 from miniamf.amf3 import IntVector, UintVector, DoubleVector, ObjectVector
 from pathlib import Path
@@ -31,6 +32,208 @@ def traceFilePath_Flash(sysverOverride: tuple = None):
         return f'C:/Users/{user}/AppData/Roaming/Macromedia/Flash Player/Logs/flashlog.txt'
     if as3state.platform == 'Darwin':
         return f'/Users/{user}/Library/Preferences/Macromedia/Flash Player/Logs/flashlog.txt'
+
+
+class _config:
+    '''
+    This class holds all config options for this library.
+    '''
+    @property
+    def hasDependencies(self):
+        return self._hasDependencies
+
+    @hasDependencies.setter
+    def hasDependencies(self, value):
+        self._hasDependencies = bool(value)
+
+    @property
+    def addedFeatures(self):
+        # Enables features added by this library.
+        return self._addedFeatures
+
+    @addedFeatures.setter
+    def addedFeatures(self, value):
+        self._addedFeatures = bool(value)
+
+    @property
+    def flashVersion(self):
+        # This currently doesn't do anything
+        # [majorVersion,minorVersion,buildNumber,internalBuildNumber]
+        return self._flashVersion
+
+    @flashVersion.setter
+    def flashVersion(self, value):
+        self._flashVersion = value
+
+    @property
+    def ErrorReportingEnable(self):
+        # Enables logging of errors (console output seems to always be active in the debugger)
+        return self._ErrorReportingEnable
+
+    @ErrorReportingEnable.setter
+    def ErrorReportingEnable(self, value):
+        self._ErrorReportingEnable = bool(value)
+
+    @property
+    def MaxWarnings(self):
+        # Number of warnings to log before stopping.
+        return self._MaxWarnings
+
+    @MaxWarnings.setter
+    def MaxWarnings(self, value):
+        self._MaxWarnings = bool(value)
+
+    @property
+    def TraceOutputFileEnable(self):
+        # Enables trace logging (console output is always be active in the debugger)
+        return self._TraceOutputFileEnable
+
+    @TraceOutputFileEnable.setter
+    def TraceOutputFileEnable(self, value):
+        self._TraceOutputFileEnable = bool(value)
+
+    @property
+    def TraceOutputFileName(self):
+        # Path to the log
+        return self._TraceOutputFileName
+
+    @TraceOutputFileName.setter
+    def TraceOutputFileName(self, value):
+        if value == '' or Path(value).is_dir():
+            print('[as3lib] Info: Using defualt TraceOutputFileName')
+            value = as3state.librarydirectory / 'flashlog.txt'
+        self._TraceOutputFileName = Path(value)
+
+    def __init__(self):
+        configpath = as3state.librarydirectory / 'as3lib.toml'
+        if configpath.exists():
+            modified = False
+            with configpath.open('rb') as f:
+                temp = TOML.readFile(f)
+            self._cfg = temp
+            tempmm = temp.get('mm.cfg', {})
+            cfg = {
+                'version': int(temp.get('version', as3state.__version__)),
+                'migrateOldConfig': bool(temp.get('migrateOldConfig', False)),
+                'dependenciesPassed': bool(temp.get('dependenciesPassed', False)),
+                'addedFeatures': bool(temp.get('addedFeatures', False)),
+                'flashVersion': tuple(temp.get('flashVersion', (32, 0, 0, 371))),
+                'mm.cfg': {
+                    'ErrorReportingEnable': bool(tempmm.get('ErrorReportingEnable', False)),
+                    'MaxWarnings': int(tempmm.get('MaxWarnings', 100)),
+                    'TraceOutputFileEnable': bool(tempmm.get('TraceOutputFileEnable', False)),
+                    'TraceOutputFileName': str(tempmm.get('TraceOutputFileName', ''))
+                }
+            }
+        else:
+            modified = True
+            self._cfg = None
+            cfg = {
+                'version': as3state.__version__,
+                'migrateOldConfig': True,
+                'dependenciesPassed': False,
+                'addedFeatures': False,
+                'flashVersion': (32, 0, 0, 371),  # I chose this version because it was the last version of flash before adobe's timebomb
+                'mm.cfg': {
+                    'ErrorReportingEnable': False,
+                    'MaxWarnings': 100,
+                    'TraceOutputFileEnable': False,
+                    'TraceOutputFileName': ''
+                }
+            }
+        if cfg['migrateOldConfig']:
+            modified = True
+            mmcfgpath = as3state.librarydirectory / 'mm.cfg'
+            wlcfgpath = as3state.librarydirectory / 'wayland.cfg'
+            oldcfgpath = as3state.librarydirectory / 'as3lib.cfg'
+            if mmcfgpath.exists():
+                with open(mmcfgpath, 'r') as f:
+                    if hasattr(configparser, 'UNNAMED_SECTION'):
+                        UNNAMED_SECTION = configparser.UNNAMED_SECTION
+                        mmcfg = configparser.ConfigParser(allow_unnamed_section=True)
+                        mmcfg.read_file(f)
+                    else:  # Python < 3.13 compatibility
+                        UNNAMED_SECTION = 'UNNAMED_SECTION'
+                        mmcfg = configparser.ConfigParser()
+                        mmcfg.read_string('[UNNAMED_SECTION]\n' + f.read())
+                cfg['mm.cfg'] = {
+                    'ErrorReportingEnable': mmcfg.getint(UNNAMED_SECTION, 'ErrorReportingEnable', fallback=0) == 1,
+                    'MaxWarnings': mmcfg.getint(UNNAMED_SECTION, 'MaxWarnings', fallback=100),
+                    'TraceOutputFileEnable': mmcfg.getboolean(UNNAMED_SECTION, 'TraceOutputFileEnable', fallback=0) == 1,
+                    'TraceOutputFileName': mmcfg.get(UNNAMED_SECTION, 'TraceOutputFileName', fallback='')
+                }
+                del mmcfg
+            if wlcfgpath.exists():
+                wlcfgpath.unlink(missing_ok=True)
+            if oldcfgpath.exists():
+                oldcfg = configparser.ConfigParser()
+                with open(oldcfgpath, 'r') as f:
+                    oldcfg.read_file(f)
+                cfg = {
+                    'version': as3state.__version__,
+                    'migrateOldConfig': False,
+                    'dependenciesPassed': False,
+                    'addedFeatures': False,
+                    'flashVersion': (32, 0, 0, 371),
+                    'mm.cfg': {
+                        'ErrorReportingEnable': oldcfg.getboolean('mm.cfg', 'ErrorReportingEnable', fallback=False),
+                        'MaxWarnings': 100,  # Reset value because I messed up the type
+                        'TraceOutputFileEnable': oldcfg.getboolean('mm.cfg', 'TraceOutputFileEnable', fallback=False),
+                        'TraceOutputFileName': oldcfg.get('mm.cfg', 'TraceOutputFileName', fallback='')
+                    }
+                }
+                oldcfgpath.unlink(missing_ok=True)
+            cfg['mm.cfg']['TraceOutputFileName'] = cfg['mm.cfg']['TraceOutputFileName'].strip('\'"')  # Sometimes the value's quotes are left in the string
+            cfg['migrateOldConfig'] = False
+        # Load some values into global state
+        self.addedFeatures = cfg['addedFeatures']
+        self.hasDependencies = True if cfg['dependenciesPassed'] and cfg['version'] == as3state.__version__ else self._dependencyCheck()
+        self.flashVersion = cfg['flashVersion']
+        self.ErrorReportingEnable = cfg['mm.cfg']['ErrorReportingEnable']
+        self.MaxWarnings = cfg['mm.cfg']['MaxWarnings']
+        self.TraceOutputFileEnable = cfg['mm.cfg']['TraceOutputFileEnable']
+        self.TraceOutputFileName = cfg['mm.cfg']['TraceOutputFileName']
+        if self.TraceOutputFileName.exists():
+            self.TraceOutputFileName.unlink()
+        self.Save(modified)
+
+    @staticmethod
+    def _dependencyCheck():
+        hasDeps = True
+        if find_spec('numpy') is None:  # https://pypi.org/project/numpy
+            as3state.initerror.append('Dependencies/Python: "numpy" not found')
+            hasDeps = False
+        if find_spec('PIL') is None:  # https://pypi.org/project/Pillow
+            as3state.initerror.append('Dependencies/Python: "Pillow" not found')
+            hasDeps = False
+        if find_spec('tkhtmlview') is None:  # https://pypi.org/project/tkhtmlview
+            as3state.initerror.append('Dependencies/Python: "tkhtmlview" not found')
+            hasDeps = False
+        if find_spec('miniamf') is None:
+            as3state.initerror.append('Dependencies/Python: "Mini-AMF" or "as3lib-miniAMF" not found')
+            hasDeps = False
+        if find_spec('tomllib') is None and find_spec('tomli') is None:
+            as3state.initerror.append('Dependencies/Python: "tomllib" or "tomli" not found')
+            hasDeps = False
+        return hasDeps
+
+    def Save(self, saveAnyways: bool = False):
+        tempcfg = {
+            'version': as3state.__version__,
+            'migrateOldConfig': False,
+            'dependenciesPassed': self.hasDependencies,
+            'addedFeatures': self.addedFeatures,
+            'flashVersion': self.flashVersion,
+            'mm.cfg': {
+                'ErrorReportingEnable': self.ErrorReportingEnable,
+                'MaxWarnings': self.MaxWarnings,
+                'TraceOutputFileEnable': self.TraceOutputFileEnable,
+                'TraceOutputFileName': self.TraceOutputFileName,
+            }
+        }
+        if saveAnyways or self._cfg != tempcfg:
+            TOML.write(as3state.librarydirectory / 'as3lib.toml', tempcfg)
+            self._cfg = tempcfg
 
 
 # Initialise as3lib
@@ -65,7 +268,7 @@ if not as3state.initdone:
         as3state.initerror.append(f'Platform/{as3state.platform} has not been tested. Things may be broken')
 
     # Load the config
-    config.Load()
+    config = _config()
 
     # Display errors to user
     if as3state.initerror:
