82e5354ffb70cfce09b9db4e71bc9cdba7c65eb7
ajdelguidice <67966644+ajdelguidice@users.noreply.github.com>
2026-08-19 19:52:41 -0500

    [cfail.cmath] Remove resizefont
    No longer needed

--- as3lib/cfail/cmath.py
+++ as3lib/cfail/cmath.py
@@ -2,7 +2,3 @@ def calculate(neww: float, newh: float, startw: int, starth: int):
     xmult = neww / startw
     ymult = newh / starth
     return ymult if xmult > ymult else xmult
-
-
-def resizefont(font: int, mult: float):
-    return round((font * mult) / 100)
