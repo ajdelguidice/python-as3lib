fcff2bf7a843e4d4c0b74f1a1006518a0cf5441f
ajdelguidice <67966644+ajdelguidice@users.noreply.github.com>
2026-08-24 19:09:48 -0500

    [tests.test_AMF] Add stub

---
+++ as3lib/tests/test_AMF.py
@@ -0,0 +1,33 @@
+from as3lib.tests import as3libTestCase, TestNotImplemented
+
+
+class AMFTests(as3libTestCase):
+    def test_array_serialization(self):
+        raise TestNotImplemented
+
+    def test_custom_obj(self):
+        raise TestNotImplemented
+
+    def test_dictionary(self):
+        raise TestNotImplemented
+
+    def test_function(self):
+        raise TestNotImplemented
+
+    def test_invalid_date(self):
+        raise TestNotImplemented
+
+    def test_missing_prop(self):
+        raise TestNotImplemented
+
+    def test_nondynamic_function_prop(self):
+        raise TestNotImplemented
+
+    def test_setter_error(self):
+        raise TestNotImplemented
+
+    def test_vector(self):
+        raise TestNotImplemented
+
+    def test_xml(self):
+        raise TestNotImplemented
